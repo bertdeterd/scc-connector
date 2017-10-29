f174cafb172970dce76d4d116ac46f1e8d3bc58e
bertdeterd <bertdeterd@creetion.com>
2017-10-29 11:37:13 +0100

    added sap-connectivity-authentication

--- index.js
+++ index.js
@@ -6,20 +6,31 @@ module.exports = {
     const axios = require("axios");
 
     const vcap_services = JSON.parse(process.env.VCAP_SERVICES);
-    const vcap_conn = vcap_services["connectivity"][0];
+    const vcap_con = vcap_services["connectivity"][0];
+    const vcap_uaa = vcap_services["xsuaa"][0];
+
     const virtual_scc_hosts = JSON.parse(process.env.SAP_SCC_VIRTUAL_HOSTS);
     const virtual_scc_host = virtual_scc_hosts[0];
-   
-    const oauth2 = new OAuth2(
-      vcap_conn.credentials.clientid,
-      vcap_conn.credentials.clientsecret,
-      vcap_conn.credentials.url + "/",
+
+    const oauthcon = new OAuth2(
+      vcap_con.credentials.clientid,
+      vcap_con.credentials.clientsecret,
+      vcap_con.credentials.url + "/",
       null,
       "oauth/token",
       null
     );
 
-    oauth2.getOAuthAccessToken(
+    const oauthuaa = new OAuth2(
+      vcap_uaa.credentials.clientid,
+      vcap_uaa.credentials.clientsecret,
+      vcap_uaa.credentials.url + "/",
+      null,
+      "oauth/token",
+      null
+    );
+
+    oauthcon.getOAuthAccessToken(
       "",
       { grant_type: "client_credentials" },
       function(e, access_token, refresh_token, results) {
@@ -27,12 +38,22 @@ module.exports = {
         axios.defaults.headers.common["Proxy-Authorization"] =
           "Bearer " + access_token;
         axios.defaults.proxy = {
-          host: vcap_conn.credentials.onpremise_proxy_host,
-          port: vcap_conn.credentials.onpremise_proxy_port
+          host: vcap_con.credentials.onpremise_proxy_host,
+          port: vcap_con.credentials.onpremise_proxy_port
         };
-        req.axios = axios;
-        req.info = vcap_services;
-        next();
+
+        oauthuaa.getOAuthAccessToken(
+          "",
+          { grant_type: "client_credentials" },
+          function(e, access_token, refresh_token, results) {
+            axios.defaults.headers.common["SAP-Connectivity-Authentication"] =
+              "Bearer " + access_token;
+
+            req.axios = axios;
+            next();
+          }
+        );
+
       }
     );
   }
